e9cc294ab7a369e94dc25cb9931e6a04b7dfdf59
Romain Hurtrelle <hurtrelleromainpro@outlook.fr>
2023-07-20 15:52:05 +0200

    commit code suite2

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
-
-import '../models/note.dart';
+import 'package:notepad_flutter/models/note.dart';
 
 class EditScreen extends StatefulWidget {
   final Note? note;
-  const EditScreen({super.key, this.note});
+  const EditScreen({Key? key, this.note}) : super(key: key);
 
   @override
   State<EditScreen> createState() => _EditScreenState();
 }
 
 class _EditScreenState extends State<EditScreen> {
-  TextEditingController _titleController = TextEditingController();
-  TextEditingController _contentController = TextEditingController();
 
-  @override
+   TextEditingController _titleController = TextEditingController();
+   TextEditingController _contentController = TextEditingController();
+
+@override
   void initState() {
     // TODO: implement initState
-    if (widget.note != null) {
-      _titleController = TextEditingController(text: widget.note!.title);
-      _contentController = TextEditingController(text: widget.note!.content);
-    }
-
+  if (widget.note != null) {
+    _titleController = TextEditingController(text: widget.note!.title);
+    _contentController = TextEditingController(text: widget.note!.content);
+  }
     super.initState();
   }
 
@@ -31,64 +30,63 @@ class _EditScreenState extends State<EditScreen> {
       backgroundColor: Colors.grey.shade900,
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  padding: const EdgeInsets.all(0),
-                  icon: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade800.withOpacity(.8),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: const Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Colors.white,
-                    ),
-                  ))
-            ],
-          ),
-          Expanded(
-              child: ListView(
-            children: [
-              TextField(
-                controller: _titleController,
-                style: const TextStyle(color: Colors.white, fontSize: 30),
-                decoration: const InputDecoration(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    padding: const EdgeInsets.all(0),
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade800.withOpacity(.8),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                      ),
+                    ))
+              ],
+            ),
+            Expanded(child: ListView(
+              children: [
+                TextField(
+                  controller: _titleController,
+                  style: const TextStyle(color: Colors.white, fontSize: 30),
+                  decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: 'Title',
-                    hintStyle: TextStyle(color: Colors.grey, fontSize: 30)),
-              ),
-              TextField(
-                controller: _contentController,
-                style: const TextStyle(
-                  color: Colors.white,
+                    hintText: 'Titre',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 30)
+                  ),
                 ),
-                maxLines: null,
-                decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Type something here',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    )),
-              ),
-            ],
-          ))
-        ]),
+                TextField(
+                  controller: _contentController,
+                  style: const TextStyle(color: Colors.white),
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Ecrivez ici',
+                      hintStyle: TextStyle(color: Colors.grey)
+                  ),
+                ),
+              ],
+            ))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(
               context, [_titleController.text, _contentController.text]);
-        },
+      },
         elevation: 10,
-        backgroundColor: Colors.grey.shade800,
-        child: const Icon(Icons.save),
+        backgroundColor: Colors.orange.shade300,
+        child: const Icon(Icons.save, color: Colors.black),
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,7 @@
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:notepad_flutter/constants/colors.dart';
+import 'package:notepad_flutter/screens/edit.dart';
 
 import '../models/note.dart';
 
@@ -25,204 +23,272 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   List<Note> sortNotesByModifiedTime(List<Note> notes) {
-    if(sorted) {
-      notes.sort((a,b) =>  a.modifiedTime.compareTo(b.modifiedTime));
+    if (sorted) {
+      notes.sort((a, b) => a.modifiedTime.compareTo(b.modifiedTime));
     } else {
-      notes.sort((b,a) =>  a.modifiedTime.compareTo(b.modifiedTime));
+      notes.sort((b, a) => a.modifiedTime.compareTo(b.modifiedTime));
     }
 
-    sorted=!sorted;
+    sorted = !sorted;
     return notes;
   }
 
-
-
- /** getRandomColor(){
-    Random random = Random();
-    return backgroundColors[random.nextInt(backgroundColors.length)];
-  }
-*/
+  /** getRandomColor(){
+      Random random = Random();
+      return backgroundColors[random.nextInt(backgroundColors.length)];
+      }
+   */
 
   void onSearchTextChanged(String searchText) {
     setState(() {
       filterNotes = sampleNotes
           .where((note) =>
-          note.content.toLowerCase().contains(searchText.toLowerCase()) ||
+      note.content.toLowerCase().contains(searchText.toLowerCase()) ||
           note.title.toLowerCase().contains(searchText.toLowerCase()))
           .toList();
     });
   }
 
-  void deleteNote(int index){
+  void deleteNote(int index) {
     setState(() {
+      Note note = filterNotes[index];
+      sampleNotes.remove(note);
       filterNotes.removeAt(index);
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(16,40,16,0),
+        padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
         child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Mes notes',
-                    style: TextStyle(fontSize: 30, color: Colors.white),),
-                  IconButton(onPressed: (){
-                    setState(() {
-                      filterNotes = sortNotesByModifiedTime(filterNotes);
-                    });
-                  },
-                      padding: EdgeInsets.all(0),
-                      icon: Container(
-                        width: 40,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade800.withOpacity(.8),
-                            borderRadius: BorderRadius.circular(10)),
-                          child: Icon(
-                            Icons.sort,
-                            color: Colors.white,
-                          ),
-                      )
-                  )
-                ],
-              ),
-              SizedBox(height: 20,),
-              TextField(
-                onChanged: onSearchTextChanged,
-                style: TextStyle(fontSize: 16,color: Colors.white),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 12),
-                  hintText: "Rechercher",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey,
-                  ),
-                  fillColor: Colors.grey.shade800,
-                  filled: true,
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(color: Colors.transparent)),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(color: Colors.transparent)),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Mes notes',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
                 ),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        filterNotes = sortNotesByModifiedTime(filterNotes);
+                      });
+                    },
+                    padding: const EdgeInsets.all(0),
+                    icon: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade800.withOpacity(.8),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Icon(
+                        Icons.sort,
+                        color: Colors.white,
+                      ),
+                    ))
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: onSearchTextChanged,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+                hintText: "Rechercher",
+                hintStyle: const TextStyle(color: Colors.grey),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                fillColor: Colors.grey.shade800,
+                filled: true,
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.transparent)),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.transparent)),
               ),
-              Expanded(
-                  child: ListView.builder(
-                    padding: EdgeInsets.only(top: 40),
-                    itemCount: filterNotes.length,
-                    itemBuilder: (context,index) {
-                      return
-                        Card(
-                          margin: const EdgeInsets.only(bottom: 30),
-                          color: Colors.orange.shade300,
-                          elevation: 3,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListTile(
-                              title: RichText(
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                text: TextSpan(
-                                    text: "${filterNotes[index].title} \n",
-                                    style: TextStyle(
+            ),
+            Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.only(top: 40),
+                  itemCount: filterNotes.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: const EdgeInsets.only(bottom: 30),
+                      color: Colors.orange.shade300,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          onTap:() async {
+                            final result = await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                 EditScreen(note: filterNotes[index]),
+                              ),
+                            );
+                            if (result!=null) {
+                              setState(() {
+                               int originalIndex = sampleNotes.indexOf(filterNotes[index]);
+                                sampleNotes[originalIndex] = Note(
+                                    id: sampleNotes[originalIndex].id,
+                                    title: result[0],
+                                    content: result[1],
+                                    modifiedTime: DateTime.now());
+
+                                filterNotes[index] = Note(
+                                    id: filterNotes[originalIndex].id,
+                                    title: result[0],
+                                    content: result[1],
+                                    modifiedTime: DateTime.now());
+
+                              });
+                            }
+                          },
+                          title: RichText(
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            text: TextSpan(
+                                text: "${filterNotes[index].title} \n",
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    height: 1.5),
+                                children: [
+                                  TextSpan(
+                                    text: "${filterNotes[index].content}",
+                                    style: const TextStyle(
                                         color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 14,
                                         height: 1.5),
-                                    children: [
-                                      TextSpan(
-                                        text: "${filterNotes[index].content}",
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.normal,
-                                            fontSize: 14,
-                                            height: 1.5),
-                                      ),
-                                    ]
-                                ),
-                              ),
-                              subtitle: Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: Text('Edité :  ${DateFormat('EEE MMM d,yyyy h:mm a').format(filterNotes[index].modifiedTime)}',
-                                  style: TextStyle(
-                                      fontSize: 10,
-                                      fontStyle: FontStyle.italic,
-                                      color: Colors.grey.shade800
                                   ),
-                                ),
-                              ),
-                              trailing: IconButton(onPressed: () {
-                                showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        backgroundColor: Colors.grey.shade900,
-                                        icon: Icon(Icons.info, color: Colors.grey,),
-                                        title: Text("Voulez-vous vraiment effacer cette note?",
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                        content: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                          ElevatedButton(
-                                            onPressed: (){
-                                            Navigator.pop(context,true);
-                                          },
-                                            style: ElevatedButton.styleFrom(
-                                              backgroundColor: Colors.green),
-                                            child: SizedBox(
-                                              width: 60,
-                                            child: Text(
-                                              "Oui",
-                                              textAlign: TextAlign.center,
-                                              style: TextStyle(color: Colors.white),
-                                            ),
-                                          )),
-                                          ElevatedButton(onPressed: (){
-                                            Navigator.pop(context,false);
-                                          },
-                                            style: ElevatedButton.styleFrom(
-                                                backgroundColor: Colors.red),
-                                            child: SizedBox(
-                                              width: 60,
-                                              child: Text(
-                                                "Non",
-                                                textAlign: TextAlign.center,
-                                                style: TextStyle(color: Colors.white),
-                                              ),
-                                            )),
-                                        ]),
-                                      );
-                                    });
-                                deleteNote(index);
-                              },icon: Icon(
-                                Icons.delete,
-                              ),
-                              ),
+                                ]),
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'Edité :  ${DateFormat('EEE MMM d,yyyy h:mm a').format(filterNotes[index].modifiedTime)}',
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontStyle: FontStyle.italic,
+                                  color: Colors.grey.shade800),
                             ),
                           ),
-                        );
-                },
-               )
-              )
-            ],
+                          trailing: IconButton(
+                            onPressed: () async {
+                              final result = await ConfirmDialog(context);
+                              if (result!=null && result) {
+                                deleteNote(index);
+                              }
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ))
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed:() {},
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+         final result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (BuildContext context) => const EditScreen(),
+              ),
+          );
+
+         if (result!=null) {
+           setState(() {
+             sampleNotes.add(Note(
+                 id: sampleNotes.length,
+                     title: result[0],
+                     content: result[1],
+                     modifiedTime: DateTime.now()));
+             filterNotes = sampleNotes;
+           });
+         }
+        },
+
         elevation: 10,
         backgroundColor: Colors.yellow.shade800,
-        child: Icon(Icons.post_add, size: 34,color: Colors.black,),
+        child: const Icon(
+          Icons.post_add,
+          size: 34,
+          color: Colors.black,
+        ),
       ),
+    );
+  }
+
+  Future<dynamic> ConfirmDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.grey.shade900,
+            icon: const Icon(
+              Icons.info,
+              color: Colors.grey,
+            ),
+            title: const Text(
+              "Voulez-vous vraiment effacer cette note?",
+              style: TextStyle(color: Colors.white),
+            ),
+            content: Row(
+                mainAxisAlignment:
+                MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context, true);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
+                      child: const SizedBox(
+                        width: 60,
+                        child: Text(
+                          "Oui",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white),
+                        ),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context, false);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red),
+                      child: const SizedBox(
+                        width: 60,
+                        child: Text(
+                          "Non",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white),
+                        ),
+                      )),
+                ]),
+          );
+        }
     );
   }
 }
